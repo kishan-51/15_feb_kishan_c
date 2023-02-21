bee0e561dea711241f9e6142acd7f5e531cbc9fb
Kishan Savaliya <125851162+kishan-51@users.noreply.github.com>
2023-02-21 11:31:09 +0530

    Add files via upload

---
+++ assignment/address.c
@@ -0,0 +1,8 @@
+// wirte a program to  Address
+#include<stdio.h>
+void main()
+{
+    printf("40 FT MAIN ROAD,\n");
+    printf("OM NAGAR-4, BLOCK NO-405,");
+    PRINTF("MAVDI, RAJKOT-360004");
+}
