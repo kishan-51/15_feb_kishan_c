6902eb78c1506ac39bcbd9046485f2b6cd997374
Kishan Savaliya <125851162+kishan-51@users.noreply.github.com>
2023-02-27 20:05:36 +0530

    Add files via upload

---
+++ assignment/c_program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    printf("Name: Kishan Savaliya \n");
+    printf("Date of Birth: 3rd Navembar 2004 \n");
+    printf("Age: 19 Years \n");
+    printf("Address: 40 ft road, om nagar, rajkot - 360004 \n");
+    return 0;
+}
